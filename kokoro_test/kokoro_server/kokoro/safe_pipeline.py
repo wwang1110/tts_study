c8364c1e956136850ed02b871f74251ff1c655aa
Weiyu <wwang1110@gmail.com>
2025-08-27 22:01:51 -0700

    update

--- kokoro_test/kokoro_server/kokoro/safe_pipeline.py
+++ kokoro_test/kokoro_server/kokoro/safe_pipeline.py
@@ -79,7 +79,8 @@ class SafePipeline:
                         # This will download the file and cache it locally
                         hf_hub_download(
                             repo_id=self.repo_id,
-                            filename=voice_file
+                            filename=voice_file,
+                            cache_dir=self.cache_dir
                         )
                         # We don't need to load the tensor here, just ensure it's downloaded
                         logger.debug(f"Successfully preloaded voice: {voice_name}")
--- kokoro_test/kokoro_server/tts_service.py
+++ kokoro_test/kokoro_server/tts_service.py
@@ -27,7 +27,7 @@ logger = logging.getLogger(__name__)
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-from .kokoro import SafePipeline
+from kokoro import SafePipeline
 
 # Configuration
 class Config:
